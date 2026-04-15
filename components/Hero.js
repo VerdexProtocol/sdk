f59729be1d5f15103e4797e1bc6f59160d149990
Verdex <tukanglossp@gmail.com>
2026-04-15 21:12:25 +0700

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Verdex</h1>
+      <p>
+        Verdex powers the future of digital assets with speed, security,
+        and seamless decentralized innovation.
+      </p>
+    </section>
+  );
+}
